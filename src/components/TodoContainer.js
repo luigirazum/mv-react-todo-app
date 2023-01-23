41f265b097c1ac8a9c106c48b0959db89213106d
Luis Zubia <soyzubia@gmail.com>
2023-01-23 15:30:08 -0600

    Add state and render TodosList, todos state is passed as prop to the TodosList component.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import TodosList from './TodosList';
 
 class TodoContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = null;
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
   }
 
   render() {
+    const { todos } = this.state;
+
     return (
-      <>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </>
+      <div>
+        <TodosList todos={todos} />
+      </div>
     );
   }
 }
