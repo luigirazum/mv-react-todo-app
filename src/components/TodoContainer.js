c8d0feb69ccb0684824f0b0f9324ae508a76c2ae
Luis Zubia <soyzubia@gmail.com>
2023-01-23 13:05:23 -0600

    Returning a React.Fragment instead of a div.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,10 +8,10 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <>
         <h1>Hello from Create React App</h1>
         <p>I am in a React Component!</p>
-      </div>
+      </>
     );
   }
 }
