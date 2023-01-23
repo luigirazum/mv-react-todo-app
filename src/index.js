a73069c356731d872146e3cc638f895b6a08f5c8
Luis Zubia <soyzubia@gmail.com>
2023-01-23 15:28:42 -0600

    Update ReactDOM by new createRoot.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 // component file
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
