823fac5a628716a31bd29ceca20ec2778ace618c
Luis Zubia <soyzubia@gmail.com>
2023-01-23 23:10:28 -0600

    Add styles to the project importing the Add.css file.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 // component file
 import TodoContainer from './components/TodoContainer';
 
+// stylesheet
+import './App.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
