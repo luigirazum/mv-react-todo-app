9ab0c36138739fb27f00291b95f24918adc57d96
Luis Zubia <soyzubia@gmail.com>
2023-01-24 13:08:29 -0600

    Add componentDidMount to retrieve todos from an API.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -9,24 +9,14 @@ class TodoContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((response) => response.json())
+      .then((data) => this.setState({ todos: data }));
   }
 
   handleChange = (id) => {
