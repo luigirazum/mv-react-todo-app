227200095652bfc5b84fa37f96b67ff777c23f73
Luis Zubia <soyzubia@gmail.com>
2023-01-24 22:05:34 -0600

    Refactor TodosList as a function based component.

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -2,35 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
+const TodosList = (props) => {
+  const {
+    todos, handleChangeProps, deleteTodoProps, setUpdate,
+  } = props;
 
-  render() {
-    const {
-      todos,
-      handleChangeProps,
-      deleteTodoProps,
-      setUpdate,
-    } = this.props;
-
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={handleChangeProps}
-            deleteTodoProps={deleteTodoProps}
-            setUpdate={setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
 
 TodosList.propTypes = {
   todos: PropTypes.isRequired,
