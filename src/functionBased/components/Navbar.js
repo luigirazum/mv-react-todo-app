3160a878c212cff53d898e293e25b18741770aaf
Luis Zubia <soyzubia@gmail.com>
2023-01-25 11:01:44 -0600

    Generate the navbar with an array that contains the links used in the App.

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Navbar = () => (
-  <div>Hello Navbar</div>
-);
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <ul>
+        {links.map((link) => {
+          const { id, text } = link;
+          return (<li key={id}>{text}</li>);
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
