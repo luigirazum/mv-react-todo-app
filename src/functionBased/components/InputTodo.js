c4a7d94f6d949e6250e05f720be0b6a3b3248946
Luis Zubia <soyzubia@gmail.com>
2023-01-24 18:51:59 -0600

    InputTodo refactored with Hooks.

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,10 +1,46 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const InputTodo = () => <div />;
+const InputTodo = (props) => {
+  const [title, setTitle] = useState('');
 
-// InputTodo.propTypes = {
-//   addTodoProps: PropTypes.func.isRequired,
-// };
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { addTodoProps } = props;
+    if (title.trim()) {
+      addTodoProps(title);
+      setTitle('');
+    } else {
+      window[alert]('Please write item');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button
+        type="button"
+        className="input-submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
 
 export default InputTodo;
