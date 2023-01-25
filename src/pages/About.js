ab836fb82d627ccda8176341c56b93c59a8cab41
Luis Zubia <soyzubia@gmail.com>
2023-01-25 10:29:27 -0600

    Create a Page component for each Route (Home, About and NotMatch).

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+  <>
+    <div>hello from about page</div>
+  </>
+);
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TodoContainer from '../functionBased/components/TodoContainer';
+
+const Home = () => (
+  <>
+    <TodoContainer />
+  </>
+);
+
+export default Home;
