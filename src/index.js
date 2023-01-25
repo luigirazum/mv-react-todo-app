7dd153b72562390e0615eac6ce9d1656e0b5d09c
Luis Zubia <soyzubia@gmail.com>
2023-01-25 10:26:20 -0600

    Start the routing just loading the App.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 // BrowserRoutes to set routes
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-// component file
-import TodoContainer from './functionBased/components/TodoContainer';
-
-// stylesheet
-import './App.css';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <TodoContainer />
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
