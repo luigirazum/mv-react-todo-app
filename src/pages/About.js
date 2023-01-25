76c836310650461557e026e3726b7944f9e5a081
Luis Zubia <soyzubia@gmail.com>
2023-01-25 13:27:47 -0600

    About page component shows the links for the App and Author.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import { Link, Outlet } from 'react-router-dom';
+
 const About = () => (
   <>
-    <div>hello from about page</div>
+    <div>
+      <h2>About</h2>
+      <ul>
+        <li>
+          <Link to="app">About App</Link>
+        </li>
+        <li>
+          <Link to="author">About Author</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
   </>
 );
 
