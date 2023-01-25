84cb25ad7447c267df30fe90b5c39fe07e22a2d3
Luis Zubia <soyzubia@gmail.com>
2023-01-24 18:31:05 -0600

    classBased components backed up in their folder, new functionBased components folder was added to hold the project components using Hooks.

---
+++ src/functionBased/components/TodosList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+class TodosList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    const {
+      todos,
+      handleChangeProps,
+      deleteTodoProps,
+      setUpdate,
+    } = this.props;
+
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+            setUpdate={setUpdate}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+TodosList.propTypes = {
+  todos: PropTypes.isRequired,
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default TodosList;
